3ccc558367a30deff502207d86eb41deedc65063
ncskier <bcw58@cornell.edu>
2017-07-07 22:57:23 -0700

    add cloudfront distribution for logo

--- _includes/js/hover-logo.js
+++ _includes/js/hover-logo.js
@@ -6,10 +6,10 @@ let div = document.getElementById('logo-div');
 
 div.onmouseout = function () {
   let img = document.getElementById('logo');
-  img.src = "{{ '/assets/images/movie-thoughts-logo.png' | relative_url }}";
+  img.src = "https://d195nid4sd8yh0.cloudfront.net/logo/movie-thoughts-logo.png";
 };
 
 div.onmouseover = function () {
   let img = document.getElementById('logo');
-  img.src = "{{ '/assets/images/movie-thoughts-logo-animation.gif' | relative_url }}";
+  img.src = "https://d195nid4sd8yh0.cloudfront.net/logo/movie-thoughts-logo-animation.gif";
 };
